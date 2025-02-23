134e2b2f4cb3c4816343143ad14661d8b0ca5b30
Luis Pareja <lpareja@hotmail.com>
2025-02-22 21:38:46 -0400

    cambios clase Rectangulo

--- app/src/main/java/com/example/rectangulo/MainActivity.kt
+++ app/src/main/java/com/example/rectangulo/MainActivity.kt
@@ -11,10 +11,39 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        
+    }
+}
+
+class Rectangulo(var color:Int, var ancho:Int, var alto:Int) {
+    var x:Int=0
+    var y:Int=0
+
+    fun MoverArriba(){
+        if (y!=0) y-=10
+    }
+
+    fun MoverAbajo(){
+        if (y!=100) y+=10
+    }
+
+    fun MoverIzquierda(){
+        if (x!=0) x-=10
+    }
+
+    fun MoverDerecha(){
+        if (x!=100) x+=10
+    }
+
+    fun CambiarTamano(){
+        if(ancho==10){
+            ancho=5
+            alto=5
+        } else
+        {
+            ancho=10
+            alto=10
         }
     }
 }

6901f30afe8e552698302863e8ff7be3b6a4b53c
Luis Pareja <lpareja@hotmail.com>
2025-02-27 21:04:39 -0400

    video 29

--- app/src/main/java/com/example/rectangulo/MainActivity.kt
+++ app/src/main/java/com/example/rectangulo/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.rectangulo
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
@@ -8,6 +9,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,39 +18,62 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val cuadradoView: View = findViewById(R.id.rectanguloX)
-        val cuadrado: Rectangulo=Rectangulo(ContextCompat.getColor(this, R.color.red),100,100)
 
-        val buttonArriba: Button = findViewById(R.id.buttonArriba)
-        val buttonAbajo: Button = findViewById(R.id.buttonAbajo)
-        val buttonIzquierda: Button = findViewById(R.id.buttonIzquierda)
-        val buttonDerecha: Button = findViewById(R.id.buttonDerecha)
-        val buttonCambiarTamano: Button = findViewById(R.id.buttonCambiarTamano)
-        val buttonCambiarColor: Button = findViewById(R.id.buttonCambiarColor)
+        cuadradoView.post {
+            val inicialX = cuadradoView.x.toInt()
+            val inicialY = cuadradoView.y.toInt()
 
-        buttonArriba.setOnClickListener {
-            cuadrado.MoverArriba()
-            actualizarVista(cuadrado,cuadradoView)
+            val inicialAncho = cuadradoView.width.toInt()
+            val inicialAlto = cuadradoView.height.toInt()
+
+            val cuadrado: Rectangulo = Rectangulo(ContextCompat.getColor(this, R.color.red),inicialAncho,inicialAlto).apply {
+                x = inicialX
+                y = inicialY
+            }
+
+            val buttonArriba: Button = findViewById(R.id.buttonArriba)
+            val buttonAbajo: Button = findViewById(R.id.buttonAbajo)
+            val buttonIzquierda: Button = findViewById(R.id.buttonIzquierda)
+            val buttonDerecha: Button = findViewById(R.id.buttonDerecha)
+            val buttonCambiarTamano: Button = findViewById(R.id.buttonCambiarTamano)
+            val buttonCambiarColor: Button = findViewById(R.id.buttonCambiarColor)
+
+            buttonArriba.setOnClickListener {
+                cuadrado.MoverArriba()
+                actualizarVista(cuadrado,cuadradoView)
+            }
+            buttonAbajo.setOnClickListener {
+                cuadrado.MoverAbajo()
+                actualizarVista(cuadrado,cuadradoView)
+            }
+            buttonIzquierda.setOnClickListener {
+                cuadrado.MoverIzquierda()
+                actualizarVista(cuadrado,cuadradoView)
+            }
+            buttonDerecha.setOnClickListener {
+                cuadrado.MoverDerecha()
+                actualizarVista(cuadrado,cuadradoView)
+            }
+            buttonCambiarTamano.setOnClickListener {
+                cuadrado.CambiarTamano()
+                actualizarVista(cuadrado,cuadradoView)
+            }
+            buttonCambiarColor.setOnClickListener {
+                //cuadrado.color = ContextCompat.getColor(this,R.color.blue)
+                cuadrado.color=generarColorAleatorio()
+                actualizarVista(cuadrado,cuadradoView)
+            }
         }
-        buttonAbajo.setOnClickListener {
-            cuadrado.MoverAbajo()
-            actualizarVista(cuadrado,cuadradoView)
-        }
-        buttonIzquierda.setOnClickListener {
-            cuadrado.MoverIzquierda()
-            actualizarVista(cuadrado,cuadradoView)
-        }
-        buttonDerecha.setOnClickListener {
-            cuadrado.MoverDerecha()
-            actualizarVista(cuadrado,cuadradoView)
-        }
-        buttonCambiarTamano.setOnClickListener {
-            cuadrado.CambiarTamano()
-            actualizarVista(cuadrado,cuadradoView)
-        }
-        buttonCambiarColor.setOnClickListener {
-            cuadrado.color = ContextCompat.getColor(this,R.color.blue)
-            actualizarVista(cuadrado,cuadradoView)
-        }
+    }
+
+    private fun generarColorAleatorio():Int{
+        val random = Random.Default
+
+        val rojo = Random.nextInt(256)
+        val verde = Random.nextInt(256)
+        val azul = Random.nextInt(256)
+
+        return Color.rgb(rojo, verde, azul)
     }
 
     private fun actualizarVista(cuadrado:Rectangulo, cuadradoView:View){
@@ -70,19 +95,19 @@ class Rectangulo(var color:Int, var ancho:Int, var alto:Int) {
     var y:Int=0
 
     fun MoverArriba(){
-        if (y!=0) y-=10
+        if (y-10 > 0) y-=10
     }
 
     fun MoverAbajo(){
-        if (y!=1000) y+=10
+        if (y+10 < 1000) y+=10
     }
 
     fun MoverIzquierda(){
-        if (x!=0) x-=10
+        if (x-10 > 0) x-=10
     }
 
     fun MoverDerecha(){
-        if (x!=1000) x+=10
+        if (x+10 < 800) x+=10
     }
 
     fun CambiarTamano(){
